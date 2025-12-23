7191d6dccee5501f4565d2ae6db5f9d3d25648cd
bala-amypo <bala@amypo.in>
2025-12-23 04:02:41 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
@@ -3,6 +3,12 @@ package com.example.demo.servlet;
 import jakarta.servlet.http.HttpServlet;
 
 @WebServlet("hello-servlet")
-public class SimpleHelloServlet{
+public class SimpleHelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException{
+
+        res.setStatus
+
+    }
 
 }
