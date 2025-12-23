4adf57e8f3a52e1f9c516297d25b8d1b2c255e1a
bala-amypo <bala@amypo.in>
2025-12-23 04:47:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.model.Category;
+
+public interface CategoryService {
+    Category addCategory(Category category);
+    List<Category> getAllCategories();
+}
