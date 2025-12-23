283b900e60b8cd44b42166399077ad397a3a7dfb
bala-amypo <bala@amypo.in>
2025-12-23 04:33:44 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,20 +1,8 @@
 package com.example.demo.dto;
 
 public class AuthResponse {
-
-    private String message;
-    private boolean success;
-
-    public AuthResponse(String message, boolean success) {
-        this.message = message;
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public boolean isSuccess() {
-        return success;
+    public String token;
+    public AuthResponse(String token) {
+        this.token = token;
     }
 }
