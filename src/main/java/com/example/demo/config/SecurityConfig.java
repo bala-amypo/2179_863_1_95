498384d78b6abfd85e079aae3ee78d36d54552a2
bala-amypo <bala@amypo.in>
2025-12-23 08:20:02 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,26 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> {})
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/swagger-ui.html"
+                    "/auth/**",
+                    "/hello-servlet",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**"
                 ).permitAll()
-
-                .requestMatchers(
-                        "/auth/**",
-                        "/categories/**"
-                ).permitAll()
-
                 .anyRequest().authenticated()
+            )
+            .sessionManagement(sess ->
+                sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             );
 
         return http.build();
