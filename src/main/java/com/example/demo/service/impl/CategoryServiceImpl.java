804e8412c8de3cecc95d3c110b1a2aa58b131db8
bala-amypo <bala@amypo.in>
2025-12-26 09:31:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -8,23 +8,20 @@ import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-
-    private final CategoryRepository categoryRepository;
-
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
-    @Override
-    public Category addCategory(Category category) {
-        if (categoryRepository.existsByName(category.getName())) {
-            throw new RuntimeException("Category already exists");
-        }
-        return categoryRepository.save(category);
-    }
-
-    @Override
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
-    }
+ private final CategoryRepository categoryRepository;
+ public CategoryServiceImpl(CategoryRepository categoryRepository) {
+ this.categoryRepository = categoryRepository;
+ }
+ @Override
+ public Category addCategory(Category category) {
+ if (categoryRepository.existsByName(category.getName())) {
+ throw new BadRequestException("Category already exists");
+ }
+ category.validateType();
+ return categoryRepository.save(category);
+ }
+ @Override
+ public List<Category> getAllCategories() {
+ return categoryRepository.findAll();
+ }
 }
