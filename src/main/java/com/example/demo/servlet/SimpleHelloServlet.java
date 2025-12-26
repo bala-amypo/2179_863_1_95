e00af9643380596c3174e6d9c05fc4c25a805b23
bala-amypo <bala@amypo.in>
2025-12-26 10:03:51 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
@@ -1,21 +1,16 @@
 package com.example.demo.servlet;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.ServletException;
 import java.io.IOException;
 
-public class SimpleHelloServlet extends HttpServlet {
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        resp.setStatus(200);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write("Hello from Simple Servlet");
-    }
-    @Override
-    public String getServletInfo() {
-        return "SimpleHelloServlet";
+        resp.getWriter().write("Hello from Servlet");
     }
 }
