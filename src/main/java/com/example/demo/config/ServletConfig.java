7bead81ef8c51b21730615826da0ca8f1a6bd5e8
bala-amypo <bala@amypo.in>
2025-12-26 10:07:40 +0000

    User commit

--- src/main/java/com/example/demo/config/ServletConfig.java
+++ src/main/java/com/example/demo/config/ServletConfig.java
@@ -1,18 +1,9 @@
 package com.example.demo.config;
 
-import com.example.demo.servlet.SimpleHelloServlet;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
 @Configuration
+@ServletComponentScan(basePackages = "com.example.demo.servlet")
 public class ServletConfig {
-
-    @Bean
-    public ServletRegistrationBean<SimpleHelloServlet> helloServlet() {
-        return new ServletRegistrationBean<>(
-                new SimpleHelloServlet(),
-                "/hello-servlet"
-        );
-    }
 }
